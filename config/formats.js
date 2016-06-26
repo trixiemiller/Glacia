c547ebd0f19034a802493d36752fd3aa83fcb74d
Kokonoe-san <kokonoesasha@gmail.com>
2016-06-26 01:23:01 +0100

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -508,6 +508,7 @@ exports.Formats = [
 		onBegin: function () {
 			this.add("raw|Super Staff Bros. <b>MELEEEEEEEEEEEEEE</b>!!");
 			this.add('message', "SURVIVAL! GET READY FOR THE NEXT BATTLE!");
+			this.add('message', "A guide for the new movesets can be found here - http://pastebin.com/Um0MxSPw");
 
 			let globalRenamedMoves = {};
 			let customRenamedMoves = {};
